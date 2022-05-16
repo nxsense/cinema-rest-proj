aae6081b97703cc9b144d2f8685ed20e6959879a
nxsense <99249892+nxsense@users.noreply.github.com>
2022-05-16 17:32:21 +0300

    Add files via upload
    first stage

---
+++ task/src/cinema/CinemaController.java
@@ -0,0 +1,20 @@
+package cinema;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CinemaController {
+
+
+
+    @GetMapping("/seats")
+    public Cinema getSeats() {
+        return getAllSeats();
+    }
+}
+
+
